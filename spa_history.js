bb0cb13168624eb16abbdb55002618ef3e4db300
ifair <261202335@qq.com>
2018-01-31 16:59:00 +0800

    Update spa_history.js

--- spa_history.js
+++ spa_history.js
@@ -1,6 +1,6 @@
 <a href="a.html" class="api">a.html</a>
-  <a href="b.html" class="api">b.html</a>
-    <a href="c.html" class="api">c.html</a>
+<a href="b.html" class="api">b.html</a>
+<a href="c.html" class="api">c.html</a>
 
 <script type="text/javascript">
 document.querySelectorAll('.api').forEach(item=>{
@@ -15,7 +15,6 @@ window.addEventListener('popState',(e)=>{
      console.log({
        location:location.href,
       state:e.state
-     }
-     );
+     });
 },false)
 </script>
