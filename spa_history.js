ddce77d3a21cf125067cccaaff77c8a5a3e3f1df
ifair <261202335@qq.com>
2018-01-31 16:52:38 +0800

    spa_history_1.0

---
+++ spa_history.js
@@ -0,0 +1,21 @@
+<a href="a.html" class="api">a.html</a>
+  <a href="b.html" class="api">b.html</a>
+    <a href="c.html" class="api">c.html</a>
+
+<script type="text/javascript">
+document.querySelectorAll('.api').forEach(item=>{
+  item.addEventListener('click',(e)=>{
+      e.preventDefault();
+      let link = item.href;
+      history.pushState(link,null,link);
+  },false);
+});
+
+window.addEventListener('popState',(e)=>{
+     console.log({
+       location:location.href,
+      state:e.state
+     }
+     );
+},false)
+</script>
